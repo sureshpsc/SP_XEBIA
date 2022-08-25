e57a7d656ee830a28e132f623672293e21a49012
sureshpsc <112111886+sureshpsc@users.noreply.github.com>
2022-08-25 12:06:00 -0500

    Update Solution_1.go

--- Solution_1.go
+++ Solution_1.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println(CapitalizeEveryThirdAlphanumericChar(strings.ToLower("Aspiration.!!ccom")))
 }
 
+//Below function would take string as a input paramater and converts each 3rd positioned character to upper case by ignoring the special character positions.
 func CapitalizeEveryThirdAlphanumericChar(s string) string {
 	rs, len := []rune(s), 0
 	for i, r := range rs {
