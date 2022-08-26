f6e1c0c7a35f56bbabc1b8263a19ca92288248da
sureshpsc <112111886+sureshpsc@users.noreply.github.com>
2022-08-26 12:04:34 -0500

    Update Solution_2.go

--- Solution_2.go
+++ Solution_2.go
@@ -11,16 +11,21 @@ type Interface interface {
 	GetValueAsRuneSlice() []rune
 }
 
+//request struct would consist of input parameters that are required to process the requested input.
 type request struct {
 	position int
 	input    string
 	length   int
 }
 
+//GetValueAsRuneSlice would convert the input string to rune slice.
 func (req *request) GetValueAsRuneSlice() []rune {
 	return []rune(req.input)
 }
 
+//TransformRune method is used to check for each and every character in string. It checks if string is having valid character with the help of unicode library.
+//If there is a valid letter or number then it looks for every alternative third character in the string.
+//Once the third character position is found, it will make that character to capital case.
 func (req *request) TransformRune(position int) {
 	if unicode.IsLetter(rune(req.input[position])) || unicode.IsNumber(rune(req.input[position])) {
 		req.length += 1
@@ -30,16 +35,19 @@ func (req *request) TransformRune(position int) {
 	}
 }
 
+//The MapString consists of interface as a parameter and calls the both implemented methods of an interface
 func MapString(i Interface) {
 	for pos := range i.GetValueAsRuneSlice() {
 		i.TransformRune(pos)
 	}
 }
 
+//NewSkipString would take input and position as a parameters and returns the request object.
 func NewSkipString(position int, input string) request {
 	return request{position: position, input: strings.ToLower(input), length: 0}
 }
 
+//The String method is used to modify the response by providing only the required output string.
 func (req request) String() string {
 	return fmt.Sprintf("%v", req.input)
 }
